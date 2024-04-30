fa9fffd7d65dd11290c8974d9e160e942bd2aafb
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-04-30 21:42:17 +0600

    made settings responsive for mobile

--- frontend/app/navbar/SettingsBtn.jsx
+++ frontend/app/navbar/SettingsBtn.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaGear } from "react-icons/fa6";
+import Settings from "../components/settings/Settings";
+import { RxCross2 } from "react-icons/rx";
 
 const SettingsBtn = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="p-2 text-white rounded-lg bg-primary">
-      <FaGear />
+    <div>
+      <button
+        className="p-2 text-white rounded-lg bg-primary"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <FaGear />
+      </button>
+      <div
+        className={`${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } fixed top-0 left-0 w-full h-full bg-white md:hidden transition-all duration-500 ease-in-out`}
+      >
+        <button
+          className="absolute text-2xl top-3 right-3"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {" "}
+          <RxCross2 />
+        </button>
+        <Settings />
+      </div>
     </div>
   );
 };
